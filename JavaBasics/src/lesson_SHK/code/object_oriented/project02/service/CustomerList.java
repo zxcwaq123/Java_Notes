08b18e212c78912e0ff524d2f1a0ecf93846ebf7
YaguangChen <ygchen1994@gmail.com>
2022-01-02 22:57:54 +0800

    练习项目2：客户信息的增删改查

---
+++ JavaBasics/src/lesson_SHK/code/object_oriented/project02/service/CustomerList.java
@@ -0,0 +1,87 @@
+package lesson_SHK.code.object_oriented.project02.service;
+
+import lesson_SHK.code.object_oriented.project02.bean.Customer;
+
+/*
+CustomerList类的设计：用来管理Customer对象，内部使用数组管理一组Customer
+
+1. 本类封装以下信息
+    Customer [] customers; 用来保存客户对象数组
+    int total = 0; 记录已保存的客户对象的数量
+2. 该类至少提供以下构造器和方法
+public CustomerList(int totalCustomer)
+public boolean addCustomer(Customer customer)
+public boolean replaceCustomer(int index, Customer cust)
+public boolean deleteCustomer(int index)
+public Customer[] getAllCustomers()
+public Customer getCustomer(int index)
+public int getTotal()
+ */
+public class CustomerList {
+
+    private Customer[] customers;
+    private int total = 0;
+
+    public CustomerList(int totalCustomer){
+        customers = new Customer[totalCustomer];
+    }
+
+    public boolean addCustomer(Customer customer){
+        if (total >= customers.length){
+            return false;
+        }
+        customers[total++] = customer;
+        return true;
+    }
+
+    public boolean replaceCustomer(int index, Customer cust){
+        if (index >= 0 && index < total){
+            customers[index] = cust;
+            return true;
+        }else {
+            System.out.printf("找不到客户%d\n",index);
+            return false;
+        }
+    }
+
+    public boolean deleteCustomer(int index){
+        if(index >= 0 && index < total){
+            for(int i = index; i < total - 1; i++){
+                customers[i] = customers[i+1];
+            }
+            // customers[total - 1] = null;
+            // total--;                  //删减别忘了total--;
+            customers[--total] = null;
+            return true;
+        }
+        else{
+            System.out.printf("找不到客户%d\n",index);
+            return false;
+        }
+    }
+
+    public Customer[] getAllCustomers(){
+        Customer[] custs = new Customer[total];
+        for (int i = 0; i < total; i++){
+            custs[i] = customers[i];
+        }
+        return custs;
+    }
+
+    public Customer getCustomer(int index){
+        if (index >= 0 && index < total){
+            return customers[index];
+        }else {
+            System.out.printf("找不到客户%d\n",index);
+            return null;
+        }
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public int getMaxNmu(){
+        return customers.length;
+    }
+}
---
+++ JavaBasics/src/lesson_SHK/code/object_oriented/project02/util/CMUtility.java
@@ -0,0 +1,125 @@
+package lesson_SHK.code.object_oriented.project02.util;
+
+import java.util.*;
+
+/**
+ CMUtility工具类：
+ 将不同的功能封装为方法，通过直接调用方法使用它，而无需考虑具体的实现细节。
+ */
+
+public class CMUtility {
+    private static Scanner scanner = new Scanner(System.in);
+
+    /**
+     * @Description 私有方法，从键盘获取数据。
+     */
+    private static String readkeyBoard(int limit, boolean blankReturn){
+        String line = "";
+
+        while (scanner.hasNextLine()){
+            line = scanner.nextLine();
+            if(line.length() == 0){
+                if(blankReturn) return line;
+                else continue;
+            }
+
+            if(line.length() < 1 || line.length() > limit){
+                System.out.printf("输入长度(1-%d)错误，请重新输入：",limit);
+                continue;
+            }
+            break;
+        }
+        return line;
+    }
+
+
+
+    /**
+     * @Description 用于界面菜单的选择。该方法读取键盘，用户输入'1'-'5'中的任意字符，则方法返回，返回值为用户输入的字符。
+     */
+
+    public static char readMenuSelection(){
+        char c;
+        for(;;){
+            String str = readkeyBoard(1,false);
+            c = str.charAt(0);
+            if(c != '1' && c != '2' && c != '3' && c != '4' && c != '5'){
+                System.out.print("输入错误，请重新输入：");
+            }else break;
+        }
+        return c;
+    }
+
+    /**
+     * @Description 从键盘读取一个字符，并返回。
+     */
+    public static char readChar(){
+        String str = readkeyBoard(1,false);
+        return str.charAt(0);
+    }
+
+    /**
+     * @重载方法
+     * @Description 从键盘读取一个字符，并返回。如果用户不输入字符而直接回车，方法将以defaultValue作为返回值。
+     */
+    public static char readChar(char defaultValue){
+        String str = readkeyBoard(1,true);
+        return (str.length() == 0) ? defaultValue : str.charAt(0);
+    }
+
+    /**
+     * @Description 从键盘读取一个长度不超过2位的整数，并将其作为方法的返回值
+     */
+    public static int readInt(int defaultValue){
+        int n;
+        for(;;){
+            String str = readkeyBoard(2,true);
+            if(str.equals("")){
+                return defaultValue;
+            }
+
+            try{
+                n = Integer.parseInt(str);
+                break;
+            } catch (NumberFormatException e){
+                System.out.print("输入错误，请重新输入：");
+            }
+        }
+        return n;
+    }
+
+    /**
+     * @Description 从键盘读取一个长度不超过limit的字符串，并将其作为方法的返回值。
+     * 如果用户不输入字符而直接回车，方法将以defaultValue作为返回值
+     */
+    public static String readString(int limit){
+        return readkeyBoard(limit,false);
+    }
+
+    /**
+     * @重载方法
+     * @Description 从键盘读取一个长度不超过limit的字符串，并将其作为方法的返回值。
+     * 如果用户不输入字符而直接回车，方法将以defaultValue作为返回值
+     */
+    public static String readString(int limit, String defaultValue){
+        String str = readkeyBoard(limit,true);
+        return str.equals("") ? defaultValue : str;
+    }
+
+    /**
+     * @Description 用于确认选择的输入，该方法从键盘读取'Y'或'N'，并将其作为方法的返回值。
+     */
+    public static char readComfirmSelection(){
+        char c;
+        for(;;){
+            String str = readkeyBoard(1,false).toUpperCase();
+            c = str.charAt(0);
+            if(c == 'Y' || c == 'N'){
+                break;
+            }else {
+                System.out.print("输入错误，请重新输入：");
+            }
+        }
+        return c;
+    }
+}
