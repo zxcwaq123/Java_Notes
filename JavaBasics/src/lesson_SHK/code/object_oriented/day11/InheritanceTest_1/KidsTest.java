a6bb735270de125faa8906372bd2ed530b10caf1
ChenYaguang <ygchen1994@gmail.com>
2022-02-11 20:39:57 +0800

    继承性练习

---
+++ JavaBasics/src/lesson_SHK/code/object_oriented/day11/InheritanceTest_1/KidsTest.java
@@ -0,0 +1,11 @@
+package lesson_SHK.code.object_oriented.day11.InheritanceTest_1;
+
+public class KidsTest {
+    public static void main(String[] args) {
+        Kids someKid = new Kids(10);
+
+        System.out.println(someKid.getSex());
+        System.out.println(someKid.getSalary());
+        System.out.println(someKid.getYeasOld());
+    }
+}
