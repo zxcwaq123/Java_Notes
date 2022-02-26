292f97d39b47f4656d332abd7abec27c63b9efe9
YaguangChen <ygchen1994@gmail.com>
2022-02-26 19:18:23 +0800

    多态性

---
+++ JavaBasics/src/lesson_SHK/code/object_oriented/day12/polymorphismTest/Woman.java
@@ -0,0 +1,20 @@
+package lesson_SHK.code.object_oriented.day12.polymorphismTest;
+
+public class Woman extends Person{
+
+    boolean isBeauty;
+
+    public void shopping(){
+        System.out.println("购物");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("女吃");
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("女走");
+    }
+}
