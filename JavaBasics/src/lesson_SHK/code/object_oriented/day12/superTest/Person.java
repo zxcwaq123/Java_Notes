7f97ce6abdab269a63c92ee303de5abd16d87587
YaguangChen <ygchen1994@gmail.com>
2022-02-26 16:38:27 +0800

    super关键字的使用

---
+++ JavaBasics/src/lesson_SHK/code/object_oriented/day12/superTest/Person.java
@@ -0,0 +1,29 @@
+package lesson_SHK.code.object_oriented.day12.superTest;
+
+public class Person {
+    String name;
+    int age;
+    int id = 1001;
+
+    public Person(){
+
+    }
+
+    public Person(String name){
+        this.name = name;
+    }
+
+    public Person(String name, int age){
+        // this.name = name;
+        this(name);
+        this.age = age;
+    }
+
+    public void eat(){
+        System.out.println("吃饭");
+    }
+
+    public void walk(){
+        System.out.println("走路");
+    }
+}
