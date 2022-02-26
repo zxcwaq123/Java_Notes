872aaed9762f337afc8e6f6b6677141c2ea7153d
YaguangChen <ygchen1994@gmail.com>
2022-02-26 14:38:14 +0800

    方法的重写

---
+++ JavaBasics/src/lesson_SHK/code/object_oriented/day12/modifierTest2/OrderTest2.java
@@ -0,0 +1,17 @@
+package lesson_SHK.code.object_oriented.day12.modifierTest2;
+
+import lesson_SHK.code.object_oriented.day12.modifierTest.Order;
+
+public class OrderTest2 {
+    public static void main (String[] args){
+        Order order = new Order();
+
+        order.orderPublic = 1;
+
+        order.methodPublic();
+
+        //不同包下的普通类（非子类），只能调用声明为public权限的方法和属性，如下：
+        //order.orderDefault = 2;
+        //order.orderProtected = 3;
+    }
+}
